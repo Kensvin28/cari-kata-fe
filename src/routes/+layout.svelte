c03bddfca2c503a83091ed170a44fe7422816601
kensvin <kensvin28@gmail.com>
2026-04-10 16:08:39 +0700

    feat: implement open graph protocol for url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,19 @@
 	<link rel="icon" href={favicon} />
 	<title>Cari Kata</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="description" content="A simple tool to find Indonesian words based on specific criteria like prefix, length, and required letters." />
+	<meta
+		name="description"
+		content="An advanced searching tool to find Indonesian words and phrases, helpful when playing crosswords (TTS), Wordle or Katla (the Indonesian version), Scrabble, and other word games."
+	/>
+	<meta property="og:title" content="Cari Kata" />
+	<meta
+		property="og:description"
+		content="An advanced searching tool to find Indonesian words and phrases, helpful when playing crosswords (TTS), Wordle or Katla (the Indonesian version), Scrabble, and other word games."
+	/>
+	<meta property="og:image" content="https://cari-kata.kensvin28.workers.dev/header.png" />
+	<meta property="og:url" content="https://cari-kata.kensvin28.workers.dev" />
+	<meta property="og:type" content="website" />
+	<meta property="og:locale" content="id_ID" />
+	<meta property="og:locale:alternate" content="en_US" />
 </svelte:head>
 {@render children()}
